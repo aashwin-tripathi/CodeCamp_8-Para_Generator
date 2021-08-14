3ac52b13b1ec2d3b6a261601fd9174af2ccc145e
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-08-14 10:06:34 +0530

    Updates

---
+++ precache-manifest.d83f45371bf4cde59a7b6be1d2559359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae9c38dc5e3c5140ec34a80e6b9dc53",
+    "url": "/CodeCamp_8-Para_Generator/index.html"
+  },
+  {
+    "revision": "647c805cf8c0131ba624",
+    "url": "/CodeCamp_8-Para_Generator/static/css/main.9d994fcb.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/CodeCamp_8-Para_Generator/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodeCamp_8-Para_Generator/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "647c805cf8c0131ba624",
+    "url": "/CodeCamp_8-Para_Generator/static/js/main.2b4b61f5.chunk.js"
+  },
+  {
+    "revision": "dbcb198b665121d21846",
+    "url": "/CodeCamp_8-Para_Generator/static/js/runtime-main.8fbc5d19.js"
+  }
+]);
